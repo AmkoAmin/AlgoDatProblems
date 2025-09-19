613a3e57643a68c90df72654f939c01f2b188293
Amin <amin.skenderi.de@gmail.com>
2025-09-20 01:37:09 +0200

    Test

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         CoinExchange firstTest = new CoinExchange(90, new int[] {10, 20, 50});
         System.out.println("Minimale Anzahl von Muenzen in dieser Kombination: " + firstTest.calcLeastNumberOfCoins());
+        //Test
     }
 }
